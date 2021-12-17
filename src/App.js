94f0f1a32e2b089696001c5b61d95ec7070f70ca
mikailaAkeredolu <mikaila@tech552.com>
2021-12-17 08:46:46 -0500

    conditionals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+//Class based Component
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Let us get started learning react</h2>
-    </div>
-  );
+// className and htmlFor instead of class="" and for=""
+//elements must be within parent div
+//Use Fragment incase of div but will not show 
+class App extends React.Component {
+
+  render(){
+
+    const name = 'mike jones';
+    const loading = false;
+    const showName = true;
+
+
+    return (
+      <div className="App">
+        
+        {/* {loading ? <h4>Loading...</h4> : <h1>Hello {showName ? name : null}...welcome to react class components</h1>} */}
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}...welcome to react class components</h1>}
+        <h2>Goodbye</h2>
+        <label htmlFor="name">Name</label>
+
+       </div>
+     
+    );
+
+  }
+
 }
 
 export default App;

8647789b394752c6364efbebe3276870aecc465d
mikailaAkeredolu <mikaila@tech552.com>
2021-12-17 05:44:50 -0500

    intro to jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//<App /> is rendered here therefore jsx shows on home page
 ReactDOM.render(
   <React.StrictMode>
     <App />

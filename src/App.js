83c157d1947732f03c4cd33b8440e0104541b33e
mikailaAkeredolu <mikaila@tech552.com>
2021-12-17 08:28:49 -0500

    newer code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       <h2>Let us get started learning react</h2>
-      <p>Just another paragrah with more jsx</p>
-      <ExpenseItem />
     </div>
   );
 }

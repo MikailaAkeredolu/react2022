9eda2f01ffabc5779338baf6dbf6102d76650c90
mikailaAkeredolu <mikaila@tech552.com>
2021-12-17 06:22:13 -0500

    added css to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,21 @@
 - A component is just a JavaScript Function that returns html code / jsx
 - You must export components
 - You can use a component in another component by importing it
+- return ( <div> </div>) - jsx in component must be wrapped with a div
 */
 
+import './ExpenseItem.css';
+
 function ExpenseItem () {
-    return <h2>Expense Items</h2>
+    return(
+        <div className="wrapper">
+            <div>
+                <h2>Insurrection Party</h2>
+                <div>January 6, 2022</div>
+                <div>$29.99</div>
+            </div>
+        </div>
+    ); 
 }
 
 

a6c368cb807681c2f7408f5e45ca4912408711ef
mikailaAkeredolu <mikaila@tech552.com>
2021-12-17 05:53:14 -0500

    updated jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h2>Let us get started learning react</h2>
-      <p>Just another paragrah</p>
+      <p>Just another paragrah with more jsx</p>
     </div>
   );
 }

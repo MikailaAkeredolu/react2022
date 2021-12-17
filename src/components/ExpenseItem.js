6eafa16132b53f9a7c76b58136a4d29909929fba
mikailaAkeredolu <mikaila@tech552.com>
2021-12-17 06:03:08 -0500

    Created ExpenseItem component and used in in the main App component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+/*
+- Start Component names with Uppercase and camel case.
+- A component is just a JavaScript Function that returns html code / jsx
+- You must export components
+- You can use a component in another component by importing it
+*/
+
+function ExpenseItem () {
+    return <h2>Expense Items</h2>
+}
+
+
+export default ExpenseItem;
+
